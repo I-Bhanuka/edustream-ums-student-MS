b577a42c5f3c6cce030877fd533a020fd5f1fcdb
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-07 16:50:55 +0530

    test: integration with the Student MS service methods

--- src/main/java/com/example/edustream_studentMS/controller/StudentController.java
+++ src/main/java/com/example/edustream_studentMS/controller/StudentController.java
@@ -1,15 +1,21 @@
 package com.example.edustream_studentMS.controller;
 
+import com.example.edustream_studentMS.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping("/test")
     public String testEndpoint() {
-        return "Hello from Student Microservice!";
+
+        return studentService.testService();
     }
 }
