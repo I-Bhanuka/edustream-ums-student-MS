8f01772ba671b76ec7927a7eb1c01b6c113a225a
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 09:48:22 +0530

    feat: add a student create abstract method in the interface

--- src/main/java/com/example/edustream_studentMS/service/StudentService.java
+++ src/main/java/com/example/edustream_studentMS/service/StudentService.java
@@ -1,5 +1,10 @@
 package com.example.edustream_studentMS.service;
 
+import com.example.edustream_studentMS.dto.requestDTO.RegisterStudentRequestDTO;
+import com.example.edustream_studentMS.dto.responseDTO.RegisterStudentResponseDTO;
+
 public interface StudentService {
     String testService();
+
+    RegisterStudentResponseDTO registerStudent(RegisterStudentRequestDTO registerStudentRequestDTO);
 }
