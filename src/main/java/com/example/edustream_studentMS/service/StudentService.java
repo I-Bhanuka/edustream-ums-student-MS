844169aca29bdbd57b5ef89ef0544fa877bf08ee
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 13:17:44 +0530

    refactor: add pagination option

--- src/main/java/com/example/edustream_studentMS/service/StudentService.java
+++ src/main/java/com/example/edustream_studentMS/service/StudentService.java
@@ -2,11 +2,14 @@ package com.example.edustream_studentMS.service;
 
 import com.example.edustream_studentMS.dto.requestDTO.RegisterStudentRequestDTO;
 import com.example.edustream_studentMS.dto.responseDTO.RegisterStudentResponseDTO;
+import com.example.edustream_studentMS.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudentService {
     String testService();
 
     RegisterStudentResponseDTO registerStudent(RegisterStudentRequestDTO registerStudentRequestDTO);
 
-    String getAllStudents();
+    Page<Student> getAllStudents(Pageable pageable);
 }
