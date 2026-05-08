800c2d7a6a2abc505fd80ebdfa916b2a4451a5bc
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 09:49:18 +0530

    test: verify the basic functionality of the student creation implementation

--- src/main/java/com/example/edustream_studentMS/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/edustream_studentMS/service/impl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.edustream_studentMS.service.impl;
 
+import com.example.edustream_studentMS.dto.requestDTO.RegisterStudentRequestDTO;
+import com.example.edustream_studentMS.dto.responseDTO.RegisterStudentResponseDTO;
 import com.example.edustream_studentMS.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,23 @@ public class StudentServiceImpl implements StudentService {
     public String testService() {
         log.info("StudentServiceImpl: testEndpoint called");
         return "Hello from Student Microservice Service Layer!";
+    }
+
+    @Override
+    public RegisterStudentResponseDTO registerStudent(RegisterStudentRequestDTO registerStudentRequestDTO) {
+        log.info("StudentServiceImpl: createStudent called");
+
+        log.info("Student Details - Name: {}, Email: {}, DOB: {}",
+                 registerStudentRequestDTO.getFirstName() + " " + registerStudentRequestDTO.getLastName(),
+                 registerStudentRequestDTO.getEmail(),
+                 registerStudentRequestDTO.getDob());
+
+        return RegisterStudentResponseDTO.builder()
+                .studentId("20230000")
+                .firstName(registerStudentRequestDTO.getFirstName())
+                .lastName(registerStudentRequestDTO.getLastName())
+                .dob(registerStudentRequestDTO.getDob())
+                .email(registerStudentRequestDTO.getEmail())
+                .build();
     }
 }
