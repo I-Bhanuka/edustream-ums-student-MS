6d2a62e83157740d743cb2296d9c3ea7ce496e74
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-11 11:03:44 +0530

    feat: add a JPQL method to query only the limited fields of student

--- src/main/java/com/example/edustream_studentMS/repository/StudentRepository.java
+++ src/main/java/com/example/edustream_studentMS/repository/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.example.edustream_studentMS.repository;
 
+import com.example.edustream_studentMS.dto.responseDTO.LimitedStudentResponseDTO;
 import com.example.edustream_studentMS.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -26,4 +30,20 @@ public interface StudentRepository extends JpaRepository<Student, UUID> {
      * WHERE studentid = ?;
      */
     Optional<Student> findByStudentId(String studentId);
+
+    @Query("""
+        SELECT new com.example.edustream_studentMS.dto.responseDTO.LimitedStudentResponseDTO(
+            s.studentId,
+            s.firstName,
+            s.lastName,
+            s.email,
+            s.dob,
+            s.enrollmentDate,
+            s.studentStatus,
+            s.courseId
+        )
+        FROM Student s
+    """
+    )
+    Page<LimitedStudentResponseDTO> findAllWithLimitedDetails(Pageable pageable);
 }
