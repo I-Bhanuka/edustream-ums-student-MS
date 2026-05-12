b166fd6898e19bddd7a7506cfe5e31c6e69aa02d
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-12 16:45:11 +0530

    feat: improve the error message's readability

--- src/main/java/com/example/edustream_studentMS/exception/AlreadyEnrolledException.java
+++ src/main/java/com/example/edustream_studentMS/exception/AlreadyEnrolledException.java
@@ -3,6 +3,6 @@ package com.example.edustream_studentMS.exception;
 public class AlreadyEnrolledException extends ConflictException {
     public AlreadyEnrolledException(String studentId) {
 
-        super("Student " + studentId + " is already enrolled in this course.");
+        super("Student: " + studentId + " is already enrolled in this course.");
     }
 }
