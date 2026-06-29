65c2a8acdda956c41e4c436ad5aac6ca7bc44da3
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-06-29 18:24:01 +0530

    feat: exponse get sessions by convocation id endpoint

--- src/main/java/com/example/edustream_studentMS/controller/ConvocationSessionController.java
+++ src/main/java/com/example/edustream_studentMS/controller/ConvocationSessionController.java
@@ -12,10 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -36,9 +36,23 @@ public class ConvocationSessionController {
 
         ConvocationSessionResponse response = convocationSessionService.createConvocationSession(request);
 
-
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(response, "Convocation session created successfully"));
+    }
+
+
+    @GetMapping("/convocation-sesssions/{convocationId}")
+    @Operation(summary = "Get Convocation Sessions by Convocation ID", description = "Retrieves convocation sessions by convocation ID")
+    public  ResponseEntity<ApiResponse<List<ConvocationSessionResponse>>> getConvocationSessionsByConvocationId(
+            @PathVariable UUID convocationId) {
+
+        log.info("Get convocation sessions for convocation ID: {}", convocationId);
+
+        List<ConvocationSessionResponse> responseList = convocationSessionService.getConvocationSessionsByConvocationId(convocationId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(responseList, "Convocation sessions retrieved successfully"));
     }
 }
