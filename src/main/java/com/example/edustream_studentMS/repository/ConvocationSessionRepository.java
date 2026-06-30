a805cf8dd70fc3d60366abb2981406bc5df278a3
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-06-30 14:49:34 +0530

    refactor: order by the results by descending of creation order

--- src/main/java/com/example/edustream_studentMS/repository/ConvocationSessionRepository.java
+++ src/main/java/com/example/edustream_studentMS/repository/ConvocationSessionRepository.java
@@ -2,11 +2,15 @@ package com.example.edustream_studentMS.repository;
 
 import com.example.edustream_studentMS.entity.ConvocationSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ConvocationSessionRepository extends JpaRepository<ConvocationSession, UUID> {
 
+    @Query("SELECT cs FROM ConvocationSession cs " +
+            "WHERE cs.convocation.id = :convocationId " +
+            "ORDER BY cs.createdAt DESC")
     List<ConvocationSession> findByConvocationId(UUID convocationId);
 }
